e1b517e1bc58c89562f3acbacf62c8062b19c8a6
VenkataMora <skreddy1719@gmail.com>
2023-11-16 15:27:34 +0000

    updated js

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,5 @@
-var displayEl= $('.display-3');
-var leadEl=$('.lead');
 var dayEl=$('#currentDay');
-
 var currenTime=dayjs().format('dddd, MMM D');
 dayEl.text(currenTime);
+var containerEl=$('.container');
+
